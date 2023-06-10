c5d497c1d4aaee29c2b737fa1c843b22608dddb2
Viet Phan Quoc <Viet.PhanQ1@homecredit.vn>
2023-06-10 18:09:29 +0700

    demo firebase with student example

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+       apiKey: "AIzaSyApCyv2VND3eh5szl8xtZy5lx20MPceDxI",
+       authDomain: "order-a2a96.firebaseapp.com",
+       databaseURL: "https://order-a2a96-default-rtdb.firebaseio.com",
+       projectId: "order-a2a96",
+       storageBucket: "order-a2a96.appspot.com",
+       messagingSenderId: "252257493437",
+       appId: "1:252257493437:web:e7079844c57ef702b672d4",
+       measurementId: "G-BD0NPVN7PM"
+     }
+};
